2fa0765d2e5325b9884704c5750c7e0ade5a2cb6
cumasamanci <email@example.com>
2026-06-26 08:31:50 +0300

    debug(images): modify debug_controller to dump product_image schema columns

--- addons/ugurlar_images/controllers/debug_controller.py
+++ addons/ugurlar_images/controllers/debug_controller.py
@@ -7,17 +7,20 @@ class DebugImagesController(http.Controller):
     @http.route('/debug/images', type='http', auth='public', csrf=False, methods=['GET'])
     def debug_images(self, **kwargs):
         try:
-            # Query product_image table
+            # Query columns of product_image
             request.env.cr.execute("""
-                SELECT pi.id, pi.name, pi.product_tmpl_id, pi.product_variant_id, 
-                       length(pi.image_1920) as size,
-                       pt.name as template_name,
-                       pp.barcode as variant_barcode
-                FROM product_image pi
-                LEFT JOIN product_template pt ON pi.product_tmpl_id = pt.id
-                LEFT JOIN product_product pp ON pi.product_variant_id = pp.id
-                ORDER BY pi.id DESC
-                LIMIT 200
+                SELECT column_name, data_type 
+                FROM information_schema.columns 
+                WHERE table_name = 'product_image'
+            """)
+            columns = [{'column_name': r[0], 'data_type': r[1]} for r in request.env.cr.fetchall()]
+
+            # Also query some records from product_image (excluding binary data fields for now)
+            request.env.cr.execute("""
+                SELECT id, name, product_tmpl_id, product_variant_id
+                FROM product_image
+                ORDER BY id DESC
+                LIMIT 50
             """)
             rows = request.env.cr.fetchall()
             
@@ -27,27 +30,13 @@ class DebugImagesController(http.Controller):
                     'id': r[0],
                     'name': r[1],
                     'product_tmpl_id': r[2],
-                    'product_variant_id': r[3],
-                    'size': r[4],
-                    'template_name': r[5],
-                    'variant_barcode': r[6]
+                    'product_variant_id': r[3]
                 })
-            
-            # Also get some templates with their image count
-            request.env.cr.execute("""
-                SELECT pt.id, pt.name, count(pi.id)
-                FROM product_template pt
-                JOIN product_image pi ON pi.product_tmpl_id = pt.id
-                GROUP BY pt.id, pt.name
-                LIMIT 50
-            """)
-            template_rows = request.env.cr.fetchall()
-            templates = [{'id': r[0], 'name': r[1], 'image_count': r[2]} for r in template_rows]
             
             result = {
                 'status': 'success',
-                'images': data,
-                'templates_with_images': templates
+                'columns': columns,
+                'images': data
             }
             return request.make_response(
                 json.dumps(result, indent=4, default=str),
