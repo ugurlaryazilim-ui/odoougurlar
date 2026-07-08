a81dd86790d8f9ce45662ac1e963d46a1b669cca
cumasamanci <email@example.com>
2026-07-08 09:11:25 +0300

    Add feed webhook to receive facebook comments

--- addons/social_media_ai_manager/controllers/oauth_meta.py
+++ addons/social_media_ai_manager/controllers/oauth_meta.py
@@ -92,7 +92,7 @@ class MetaOAuthController(http.Controller):
                 try:
                     subscribe_url = f"https://graph.facebook.com/v19.0/{page_id}/subscribed_apps"
                     sub_data = {
-                        'subscribed_fields': 'messages,messaging_postbacks,comments',
+                        'subscribed_fields': 'messages,messaging_postbacks,comments,feed',
                         'access_token': page_token
                     }
                     sub_resp = requests.post(subscribe_url, data=sub_data).json()
